e8887ce5f4fb401398e31850d0c969baab8652f8
Nasibali <abdiyevnasibali@gmail.com>
2022-06-15 15:20:24 +0500

    Fourth Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'package:get/get.dart';
+import 'pages/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return GetMaterialApp(
       home: HomePage(),
     );
   }
---
+++ lib/pages/home/home_controller.dart
@@ -0,0 +1,39 @@
+import 'package:get/get.dart';
+import 'package:learn_getx/models/post_model.dart';
+import 'package:learn_getx/services/network_service.dart';
+import 'package:learn_getx/services/util_service.dart';
+
+class HomeController extends GetxController {
+  // fields
+  List<Post> items = [];
+  bool isLoading = false;
+
+  // logic method
+  @override
+  void onInit() {
+    super.onInit();
+    getDataFromNetwork();
+  }
+
+  void getDataFromNetwork() async {
+    isLoading = true;
+    update();
+
+    NetworkService.GET(NetworkService.API_POST, NetworkService.emptyParams()).then((response) => _checkData(response));
+  }
+
+  void _checkData(String? response) {
+    if(response != null) {
+      _showData(response);
+    } else {
+      Utils.fireSnackGetX("Please tyr again! Something went error!");
+    }
+    isLoading = false;
+    update();
+  }
+
+  void _showData(String response) {
+    items = NetworkService.parsePostList(response);
+    update();
+  }
+}
