b5cf199fdcfe595f24b91e3b2f268a19556966f0
Nasibali <abdiyevnasibali@gmail.com>
2022-06-15 15:20:32 +0500

    Fourth Commit

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learn_getx/pages/home/home_controller.dart';
+import 'package:learn_getx/views/post_card_view.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomeController>(
+      init: HomeController(),
+      builder: (controller) {
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text("POSTS"),
+            backgroundColor: Colors.blueGrey[800],
+          ),
+          body: Stack(
+            children: [
+              // #body
+              ListView.builder(
+                  itemCount: controller.items.length,
+                  itemBuilder: (context, index) {
+                    return PostCardView(post: controller.items[index]);
+                  }
+              ),
+
+              // #indicator
+              Visibility(
+                visible: controller.isLoading,
+                child: const Center(child: CircularProgressIndicator()),
+              )
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+        );
+      }
+    );
+  }
+}
